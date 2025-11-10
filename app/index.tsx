526214b3afa1536f51cd568a5a8373e222d708e9
Diego <dpadula@epe.santafe.gov.ar>
2025-11-10 13:22:08 -0300

    Subida con primer test

---
+++ app/index.tsx
@@ -0,0 +1,33 @@
+import LottieView from 'lottie-react-native';
+import { useEffect, useRef } from 'react';
+import { View } from 'react-native';
+
+export default function Index() {
+  const animation = useRef<LottieView>(null);
+  useEffect(() => {
+    // You can control the ref programmatically, rather than using autoPlay
+    // animation.current?.play();
+  }, []);
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffffff4b',
+      }}
+    >
+      <LottieView
+        autoPlay
+        ref={animation}
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: 'transparent',
+        }}
+        // Find more Lottie files at https://lottiefiles.com/featured
+        source={require('../assets/images/weather-partly-shower.json')}
+      />
+    </View>
+  );
+}
